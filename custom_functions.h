59ec7790cf5a0fbc797911facef41f65e5698cd8
Dominent <petromilpavlov@gmail.com>
2015-04-25 19:11:21 +0200

    [5]Some fixes, added fruit and fixed frame

--- custom_functions.h
+++ custom_functions.h
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
-#include <cstdlib> // exit()
-#include <windows.h> // Sleep()
+#include <cstdlib> // exit(), srand(), 
+#include <windows.h> // Sleep(), 
+#include <ctime> // time(), 
 
 
 using namespace std;
@@ -22,15 +23,49 @@ void draw_board(char playBoard[][75], int score){ //Draws a play board 23x75 big
 	}
 }
 
-void quit_game(int score, int seconds){ //Displays score and quits after x secconds.
+void quit_game(int score, int seconds){ //Displays score and quits after x secconds.( 100% Done )
 
 	system("cls");
 	cout << "GAME OVER!!!!\n\n";
 	cout << "You got a score of: " << score << '\n';
 
 	for (int i = 0; i <= seconds; ++i){
-		Sleep(1000);
+		Sleep(1000); //1000 milliseconds = 1 second
 		cout << "The program will close in: " << 5 - i << " seconds." << endl;
 	}
 	exit(0); //1 - unsuccessful program termination, 0 - successful program termination 
+}
+
+void create_frame(char playBoard[][75]){ //Creates the frame.( 100% Done )
+
+	for (int x = 0; x < 23; x++)
+	{
+		for (int y = 0; y < 75; y++)
+		{
+			if (playBoard[x][y] != '@'){
+
+				if (x == 0 || x == 22 || y == 0 || y == 74)playBoard[x][y] = 177;
+				else playBoard[x][y] = ' ';
+			}
+		}
+	}
+}
+
+void fruit(int fruitCount, char playBoard[][75]){ //Generate and set fruits at their corresponding possition. ( 100% Done )
+
+	srand(time(NULL)); //Initializing random seed generator (using time() as seed)( 100% Done. )
+	int up = 0;
+	int left = 0;
+
+	for (int x = 0; x < fruitCount; x++){
+
+		up = (rand() % 22);
+		left = (rand() % 74);
+
+		if (playBoard[up][left] == 'o') fruit(fruitCount, playBoard);
+		else if (playBoard[up][left] == '*') fruit(fruitCount, playBoard);
+		else if (playBoard[up][left] == 177) fruit(fruitCount, playBoard);
+		else playBoard[up][left] = '@';
+
+	}
 }
--- main.cpp
+++ main.cpp
@@ -12,20 +12,23 @@
 /*---------------- Function prototypes ----------------*/
 void draw_board(char playBoard[][75], int score);
 void quit_game(int score, int seconds);
+void create_frame(char playBoard[][75]);
+void fruit(int fruitCount, char playBoard[][75]);
 /*-----------------------------------------------------*/
-
-
 
 int main(){
 	int score= 0; // Stores here the score of the user.
 	char playBoard[23][75]; //Using 2D array to create a Matrix for the snake to move.
 	int secToClose = 5; //Secconds after which the program quits.
+	bool Quit = false; 
 	
-	draw_board(playBoard, score);
-		
-	
-	char ch;
-	cin >> ch;
+	while (Quit == false){ //game loop
+
+		draw_board(playBoard, score);
+		create_frame(playBoard);
+		fruit(3, playBoard);
+
+	}
 
 	quit_game(score, secToClose);
 }
